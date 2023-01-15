73539c3db25d7b88bd463d68c80996d7faffc50f
pacomesoual <pacomesoual@yahoo.fr>
2023-01-15 18:01:15 +0100

    Mise a jour de la branche,
    Ajout de textures pour deux blocs;
    Ajout d'un script de déplacement simpliste.

---
+++ Scripts/Vaisseaux/Déplacement/DeplacementGroupe.gd
@@ -0,0 +1,31 @@
+extends KinematicBody2D
+
+var speed = 5
+var velocity := Vector2(0,0) #
+var lastFrameVelocity = Vector2(0,0)
+var InputMovement = Vector2(0,0)
+
+func _ready():
+	pass
+
+func _unhandled_input(event):
+	pass
+
+func _input(event):
+	pass
+
+func _physics_process(delta):
+	
+	lastFrameVelocity = velocity #Tentative #1 du système d'inertie.
+#	Definition de InputMovement comme étant le vector2 représentant les inputs du joueur.
+	InputMovement = Vector2(0 + int(Input.is_action_pressed("right0")) - int(Input.is_action_pressed("left0")),0 + int(Input.is_action_pressed("down0")) - int(Input.is_action_pressed("up0")))
+
+#	Normalization du mouvement pour s'assurer que la vitesse ne dépasse pas :
+	if(InputMovement.length() >1):
+		InputMovement = InputMovement.normalized()
+	
+#	Application du mouvement :
+	velocity = lastFrameVelocity + InputMovement
+	self.position += velocity * 0.3
+	
+	
