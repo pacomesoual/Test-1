e05955012f1361c3a32b727bf9e56802b89902d0
pacomesoual <pacomesoual@yahoo.fr>
2023-01-16 18:31:39 +0100

    Update DeplacementGroupe.gd

--- Scripts/Vaisseaux/Déplacement/DeplacementGroupe.gd
+++ Scripts/Vaisseaux/Déplacement/DeplacementGroupe.gd
@@ -1,9 +1,9 @@
 extends KinematicBody2D
 
 var speed = 5
-var velocity := Vector2(0,0) #
-var lastFrameVelocity = Vector2(0,0)
-var InputMovement = Vector2(0,0)
+var velocity := Vector2(0,0) #vitesse appliqué a l'objet chaque frame
+var lastFrameVelocity = Vector2(0,0) #Vitesse qui a été appliqué a l'objet a la derniere frame
+var InputMovement = Vector2(0,0) #vecteur représentant l'input.
 
 func _ready():
 	pass
@@ -15,17 +15,27 @@ func _input(event):
 	pass
 
 func _physics_process(delta):
-	
-	lastFrameVelocity = velocity #Tentative #1 du système d'inertie.
 #	Definition de InputMovement comme étant le vector2 représentant les inputs du joueur.
-	InputMovement = Vector2(0 + int(Input.is_action_pressed("Krigh")) - int(Input.is_action_pressed("Kleft")),0 + int(Input.is_action_pressed("Kdown")) - int(Input.is_action_pressed("Kup")))
+	InputMovement = Vector2(0 + int(Input.is_action_pressed("Kright")) - int(Input.is_action_pressed("Kleft")),0 + int(Input.is_action_pressed("Kdown")) - int(Input.is_action_pressed("Kup")))
 
 #	Normalization du mouvement pour s'assurer que la vitesse ne dépasse pas :
 	if(InputMovement.length() >1):
 		InputMovement = InputMovement.normalized()
-	
+		
+#	Le freinage has arrived :
+	if(Input.is_action_pressed("Kbrake")):
+		if (-1 <lastFrameVelocity.length() and lastFrameVelocity.length() < 1): #check si la velocity est suffisament basse pour que quoi que ce soit a 1 créer un effet yoyo.
+			InputMovement = Vector2(-lastFrameVelocity.x,-lastFrameVelocity.y)
+		else:
+			var absolute = abs(lastFrameVelocity.x) + abs(lastFrameVelocity.y)
+			var ratioX = lastFrameVelocity.x/absolute
+			var ratioY = lastFrameVelocity.y/absolute
+			InputMovement.x = -ratioX
+			InputMovement.y = -ratioY
+
 #	Application du mouvement :
 	velocity = lastFrameVelocity + InputMovement
+	
+	lastFrameVelocity = velocity #Tentative #1 du système d'inertie.
+	
 	self.position += velocity * 0.3
-	
-	
